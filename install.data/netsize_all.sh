df9559ad2a617baae96016f7351d48beb68e8334
Rick771 <ricardo.caiza@est.ikiam.edu.ec>
2021-12-11 00:18:07 -0500

    Intro y links de referencia

--- install.data/netsize_all.sh
+++ install.data/netsize_all.sh
@@ -1,6 +1,12 @@
-## TAREA 1 BIOINFORMÁTICA
-Instalar Octave
+# TAREA 1 BIOINFORMÁTICA
+##Instalar Octave :muscle:
+
+Detallaremos algunas opciones para descargar e instalar Octave de forma gratuita en cada sistema operativo, aunque esto no es necesario si va a usarse a través de la web.
+Tienes más información sobre Octave en su web, incluyendo su manual en HTML y PDF.
+
+Para más información puede revisa r estos links:
+ - <https://sites.google.com/site/manuelmuinhospan/weboctave>
+ - <https://www.gnu.org/software/octave/download>
 
 :+1:
 :v:
-:muscle:
