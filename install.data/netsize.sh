bbade70601dfa078f64e1e2ddd5a90e5fecb63fb
Rick771 <ricardo.caiza@est.ikiam.edu.ec>
2021-12-10 19:41:33 -0500

    Links de referencia

--- install.data/netsize.sh
+++ install.data/netsize.sh
@@ -1,5 +1,10 @@
 ## TAREA 1 BIONFORMATICA
 Instalar Octave
+
 Detallaremos algunas opciones para descargar e instalar Octave de forma gratuita en cada sistema operativo, aunque esto no es necesario si va a usarse a través de la web.
 Tienes más información sobre Octave en su web, incluyendo su manual en HTML y PDF.
+
+Para más información puede revisa r estos links:
+ - <https://sites.google.com/site/manuelmuinhospan/weboctave>
+ - <https://www.gnu.org/software/octave/download>
 :+1:
