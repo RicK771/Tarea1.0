60345ac566cce12ed6534502fe8d976cf7ccfdfc
Rick771 <ricardo.caiza@est.ikiam.edu.ec>
2021-12-10 19:56:11 -0500

    Instrucciones para windows

--- install.data/netsize.sh
+++ install.data/netsize.sh
@@ -1,5 +1,5 @@
-## TAREA 1 BIONFORMATICA
-Instalar Octave
+#TAREA 1 BIONFORMATICA
+##Instalar Octave
 
 Detallaremos algunas opciones para descargar e instalar Octave de forma gratuita en cada sistema operativo, aunque esto no es necesario si va a usarse a través de la web.
 Tienes más información sobre Octave en su web, incluyendo su manual en HTML y PDF.
@@ -7,4 +7,12 @@ Tienes más información sobre Octave en su web, incluyendo su manual en HTML y 
 Para más información puede revisa r estos links:
  - <https://sites.google.com/site/manuelmuinhospan/weboctave>
  - <https://www.gnu.org/software/octave/download>
+
+###Windows
+<https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Logo_windows_simples.svg/1200px-Logo_windows_simples.svg.png>
+U Octave incorpora la versión 6.3.0 y es la opción más recomendable. Descarga octave-6.3.0-w32-installer.exe (319 Mb) u octave-6.3.0-w64-installer.exe (325 Mb), según tengas un Windows de 32 o 64 bits (si no sabes qué versión tienes, escoge la de 32), y ejecuta el archivo para instalar el programa
+Si tienes un sistema operativo antiguo, como Windows XP, la versión anterior no funcionará, pero puedes usar Octave 4.4.0 (32 bits) (198 Mb) u Octave 4.4.0 (64 bits) (205 Mb).
+Octave-Forge incorpora versiones anteriores, por si dispones de muy poco espacio. Puedes descargar Octave 3.2.0 (45 Mb) u Octave 3.6.4 (68 Mb), aunque estas versiones no incluyen un entorno con ventanas.
+También tienes una wiki que detalla otras posibilidades, y un video-tutorial que describe la instalación de Octave (y del paquete symbolic) en Windows.
+
 :+1:
