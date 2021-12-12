856f4f2d0df9d9de5fb3c5ebd175c34d255bf540
RicK771 <94474279+RicK771@users.noreply.github.com>
2021-12-11 23:07:15 -0500

    Update netsize_all.sh
    Repositorio Savedra

--- install.data/netsize_all.sh
+++ install.data/netsize_all.sh
@@ -1,6 +1,6 @@
 #TAREA 1 BIONFORMATICA - EJERCICIO 1.10.3
     ###continuación:
-###Para la ejecucion de esta seccion se hace uso dell directorio  de README.md
-
+###Para la ejecucion de esta seccion se hace uso del directorio  de README.md
+    - Elejimos la ruta del repositorio
 :+1:
 :v:
