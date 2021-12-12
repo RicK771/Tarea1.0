0fdd74144ebb23d7b4c6c0a03329845f6a5026d7
RicK771 <94474279+RicK771@users.noreply.github.com>
2021-12-11 22:55:09 -0500

    Update netsize.sh
    Intro

--- install.data/netsize.sh
+++ install.data/netsize.sh
@@ -1,5 +1,7 @@
 #TAREA 1 BIONFORMATICA
-##
-
+##Requerimentos
+  - Instalar un preograma que nos sea uti para realizar el ejercicio
+   - Definimos la ruta:
+     cd Documents/Ikiam_2021data/CSB/unix/Saavedra2013
 ###Esperamos esta informacion te sea util..!!
 :+1:
