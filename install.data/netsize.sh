0a540a2f6583d37ad69386fc8fae3187ab90307c
RicK771 <94474279+RicK771@users.noreply.github.com>
2021-12-11 22:56:19 -0500

    Update netsize.sh
    Terminal

--- install.data/netsize.sh
+++ install.data/netsize.sh
@@ -3,5 +3,8 @@
   - Instalar un preograma que nos sea uti para realizar el ejercicio
    - Definimos la ruta:
      cd Documents/Ikiam_2021data/CSB/unix/Saavedra2013
+##una vez hecho esto
+###Ingresamos terminal
+cat n1.txt
 ###Esperamos esta informacion te sea util..!!
 :+1:
