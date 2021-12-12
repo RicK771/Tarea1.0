91f2090184249bde2c3922501778c32a9e4814e7
RicK771 <94474279+RicK771@users.noreply.github.com>
2021-12-11 23:05:37 -0500

    Update netsize_all.sh
    Inicio P2

--- install.data/netsize_all.sh
+++ install.data/netsize_all.sh
@@ -1,4 +1,6 @@
-# TAREA 1 BIOINFORMÁTICA
-##
+#TAREA 1 BIONFORMATICA - EJERCICIO 1.10.3
+    ###continuación:
+###Para la ejecucion de esta seccion se hace uso dell directorio  de README.md
+
 :+1:
 :v:
